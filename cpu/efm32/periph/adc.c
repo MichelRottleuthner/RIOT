53e8fb5b9451abade5b308d509283db26019a6ff
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2022-08-03 10:58:29 +0200

    efm32/periph/adc: add custom extension to modify adc sampling properties that affect sampling speed

--- cpu/efm32/periph/adc.c
+++ cpu/efm32/periph/adc.c
@@ -32,6 +32,35 @@
 
 static mutex_t adc_lock[ADC_DEV_NUMOF];
 
+static uint32_t _min_sampling_time_ns = 1000;
+/* adcAcqTime8 is the default value of the original RIOT implementation */
+static uint32_t _acquisition_val = adcAcqTime8;
+#define ADC_DEFAULT_FREQ (400000)
+#define NS_PER_SECOND (1000000000)
+#define NS_PER_ADC_CYLCE (NS_PER_SECOND / ADC_DEFAULT_FREQ)
+/* The API currently does not support explicit setting of different sample times.
+ * This custom helper is used to inject a minimum required sample time that will
+ * be used by the default init function */
+void _adc_custom_extension_set_min_sample_time(uint32_t ns) {
+    _min_sampling_time_ns = ns;
+
+/* on other platforms this mapping might not be appliccable (not verivied) */
+#if defined(CPU_MODEL_EFM32PG12B500F1024GL125)
+    /* min: adcAcqTime1 == 0; max: adcAcqTime256 == 9 */
+    _acquisition_val = adcAcqTime256;
+    for (unsigned i = 0; i < 9; i++) {
+        /* acqisition cycles are powers of two given by 2^i */
+        unsigned cycles = 1 << i;
+        if (cycles * NS_PER_ADC_CYLCE >= _min_sampling_time_ns) {
+            _acquisition_val = i;
+            break;
+        }
+    }
+#elif
+#error "Custom ADC extension for sample time calculation not verified on this platform"
+#endif
+}
+
 int adc_init(adc_t line)
 {
     /* check if line is valid */
@@ -53,8 +82,8 @@ int adc_init(adc_t line)
     ADC_Init_TypeDef init = ADC_INIT_DEFAULT;
 
     init.timebase = ADC_TimebaseCalc(0);
-    init.prescale = ADC_PrescaleCalc(400000, 0);
 
+    init.prescale = ADC_PrescaleCalc(ADC_DEFAULT_FREQ, 0);
     ADC_Reset(adc_config[dev].dev);
     ADC_Init(adc_config[dev].dev, &init);
 
@@ -76,7 +105,7 @@ int32_t adc_sample(adc_t line, adc_res_t res)
     /* setup channel */
     ADC_InitSingle_TypeDef init = ADC_INITSINGLE_DEFAULT;
 
-    init.acqTime = adc_channel_config[line].acq_time;
+    init.acqTime = _acquisition_val;
     init.reference = adc_channel_config[line].reference;
     init.resolution = (ADC_Res_TypeDef) (res & 0x0F);
 #if defined(_SILICON_LABS_32B_SERIES_0)
