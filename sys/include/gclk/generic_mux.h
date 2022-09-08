31e748a9d396ef38a51f84776aef4b95fbed78a4
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2022-09-08 13:25:56 +0200

    sys/include/gclk/generic_mux: improve doc, drop deprecated interface declarations

--- sys/include/gclk/generic_mux.h
+++ sys/include/gclk/generic_mux.h
@@ -30,7 +30,9 @@
 extern "C" {
 #endif
 
-/* The basic mux uses the generic basic clock type directly */
+/**
+ * @brief The generic mux uses the generic basic clock type directly.
+ */
 typedef gclk_basic_clock_t gclk_mux_ll_t;
 
 /**
@@ -42,87 +44,45 @@ static inline gclk_mux_ll_t *to_gclk_mux_ll_t(const gclk_t *clk)
 }
 
 /**
- * @brief Generic implementation of the gclk_ops_t.enable interface.
+ * @brief Get the current parent of the clock.
  *
- * It is meant to be used for hardware that can be modelled with the generic gate type gclk_mux_ll_t.
- * For a mux, this function is optional because many muxes don't incorporate a gate to disable the output.
- * If the mux has it's own gate (one that only affects it's output clock) it should be preferred to directly include it
- * with the mux instance declaration to avoid overhead of a separate gate instance.
+ * @param[in] clk   The clock instance to get the parent of.
  *
- * If this function is used, the mux instance must be initialized with values for the enable register and its bit.
- * Optionally the ready register and mask definitions are used to wait for eabling/disabling to be finished.
+ * @return The pointer to the clock instance that is currently the parent of @p clk.
  */
-bool gclk_generic_mux_enable(const gclk_t *clk, gclk_enable_option_t opt);
-
-/**
- * @brief Generic implementation of the gclk_ops_t.set_parent interface.
- *
- * It is meant to be used for hardware that can be modelled with the generic mux type gclk_mux_ll_t.
- * This function is mandatory for any mux.
- *
- */
-int gclk_generic_mux_set_parent(const gclk_t *clk, unsigned int index);
-
-/**
- * @brief Generic implementation of the gclk_ops_t.get_parent interface.
- *
- * It is meant to be used for hardware that can be modelled with the generic mux type gclk_mux_ll_t.
- * This function is mandatory for any mux.
- *
- */
-const gclk_t* gclk_generic_mux_get_parent(const gclk_t *clk, unsigned int idx);
-
-/**
- * @brief Generic implementation of the gclk_ops_t.set_freq interface.
- *
- * It is meant to be used for hardware that can be modelled with the generic mux type gclk_mux_ll_t.
- * This function is optional for any mux.
- * @todo: decide if we prefere flexibility or strict semantics here.
- *        A pure mux only has the option to change it's parent to alter it's frequency. But that mixes topological
- *        changes with frequency configuration changes. (Exception: setting the parent to NULL *could* be considered
- *        a pure fequency change as no other topology is touched)
- *        Another possiblity would be to employ a "forward semantics" to the set_freq request so the next node that
- *        actually can change freq would perform an action. But that would probably need somerestirictions to only
- *        forward the request no other clock is involved (this instance is the only child/topology chain of the changed
- *        instance)
- *
- */
-unsigned long gclk_generic_mux_set_freq(const gclk_t *clk, uint32_t hz, uint32_t parent_hz);
-
-/**
- * @brief Generic implementation of the gclk_ops_t.get_freq interface.
- *
- * It is meant to be used for hardware that can be modelled with the generic mux type gclk_mux_ll_t.
- * This function is mandatory for any mux.
- * It returns the frequency of the currently selected clock.
- */
-unsigned long gclk_generic_mux_get_freq(const gclk_t *clk);
-
-/**
- * @brief Generic implementation of the gclk_ops_t.check_freq interface.
- *
- * It is meant to be used for hardware that can be modelled with the generic mux type gclk_mux_ll_t.
- * This function is mandatory for any mux.
- * This function does'not query or alter HW state and is meant for apriori evaluation of freq configurations.
- */
-unsigned long gclk_generic_mux_check_freq(const gclk_t *clk,
-                                                      clk_topology_entry_t *input_topology,
-                                                      uint32_t topology_len, uint32_t hz,
-                                                      uint32_t flags);
-
 const gclk_t* gclk_plain_mux_get_parent(const gclk_t *clk);
+
+/**
+ * @brief Set the parent of a clock via the index.
+ *
+ * @param[in] clk   The clock instance to set the parent of.
+ * @param[in] idx   The index that refers to a parent (specific to @p clk).
+ */
 void gclk_plain_mux_set_parent(const gclk_t *clk, unsigned int idx);
 
-/* Implementation of only the scale_ops interface for a scaler that is interfaced
- * via a bitfield in a read/write register that exposes the current selection while
- * also allowing to change it via that bitfield.
+/**
+ * @brief Implementation of only the @ref gclk_mux_ops_t interface.
+ *
+ * Used for mux instances that are interfaced via a bitfield in a read/write
+ * register that exposes the current selection while also allowing to change
+ * it via that bitfield.
+ *
  * @note The register access MUST be encoded in a way understandable by the
  * generic mux implementation (regref). If that is not possible on a specific platform,
- * custom ops must be used instead */
+ * custom ops must be used instead.
+ */
 extern const gclk_op_t gclk_plain_mux_ops[];
 
-/* This helper is meant for static initialization for plain mux instances that are compatible
- * with the backend provided by the generic mux implementation */
+/**
+ * @brief Static initialization helper for plain mux instances.
+ *
+ * Meant to be used for muxes that are compatible to the
+ * @ref gclk_plain_mux_ops implementation.
+ *
+ * @param NAME           A unique char string name for the clock instance (without quotes!).
+ * @param CONF_REG_ID    LUT ID of the config register that is used for selecting the parent.
+ * @param CONF_REG_MASK  Mask of all bits responsible for the parent config setting.
+ */
 #define GCLK_PLAIN_MUX_STATIC_INIT(NAME,CONF_REG_ID,CONF_REG_MASK)\
 .base.separated_ops  = gclk_plain_mux_ops,\
 .base.flags.muxable  = 1,\
