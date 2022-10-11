35c79c4631e40adf77b76678f945658c4d5c95d5
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2022-10-11 17:52:15 +0200

    sys/shell: add gclk cmds to default shell commands if gclk module is enabled, add shell_cmd_gclk to pseudomodules

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -307,6 +307,7 @@ PSEUDOMODULES += shell_cmd_cord_ep
 PSEUDOMODULES += shell_cmd_cryptoauthlib
 PSEUDOMODULES += shell_cmd_dfplayer
 PSEUDOMODULES += shell_cmd_fib
+PSEUDOMODULES += shell_cmd_gclk
 PSEUDOMODULES += shell_cmd_gnrc_icmpv6_echo
 PSEUDOMODULES += shell_cmd_gnrc_ipv6_blacklist
 PSEUDOMODULES += shell_cmd_gnrc_ipv6_frag_stats
