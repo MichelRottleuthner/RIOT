0e318493b2c1731b58191d8bf2b51661ac90ae0c
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2023-08-15 19:33:36 +0200

    examples/opendsme: add conditional init of pins for LA debugging

--- examples/opendsme/main.c
+++ examples/opendsme/main.c
@@ -28,6 +28,41 @@ static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
 int main(void)
 {
+#ifdef LA_PIN2
+    gpio_init(LA_PIN2, GPIO_OUT);
+    gpio_clear(LA_PIN2);
+#endif
+
+#ifdef LA_PIN3
+    gpio_init(LA_PIN3, GPIO_OUT);
+    gpio_clear(LA_PIN3);
+#endif
+
+#ifdef LA_PIN6
+    gpio_init(LA_PIN6, GPIO_OUT);
+    gpio_clear(LA_PIN6);
+#endif
+
+#ifdef LA_PIN12
+    gpio_init(LA_PIN12, GPIO_OUT);
+    gpio_clear(LA_PIN12);
+#endif
+
+#ifdef LA_PIN13
+    gpio_init(LA_PIN13, GPIO_OUT);
+    gpio_clear(LA_PIN13);
+#endif
+
+#ifdef LA_PIN14
+    gpio_init(LA_PIN14, GPIO_OUT);
+    gpio_clear(LA_PIN14);
+#endif
+
+#ifdef LA_PIN15
+    gpio_init(LA_PIN15, GPIO_OUT);
+    gpio_clear(LA_PIN15);
+#endif
+
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
 
     gnrc_netreg_entry_t dump = GNRC_NETREG_ENTRY_INIT_PID(GNRC_NETREG_DEMUX_CTX_ALL,
