ff5cb7e18126fc0043a2bd553fb7f08873011b5a
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2023-09-03 11:57:08 +0200

    examples/gnrc_networking/main: configure debug pins and network interface on startup

--- examples/gnrc_networking/main.c
+++ examples/gnrc_networking/main.c
@@ -22,16 +22,106 @@
 
 #include "shell.h"
 #include "msg.h"
+#include "periph/gpio.h"
+#include "net/gnrc/netapi.h"
+#include "net/gnrc/netif.h"
+#include "periph/cpuid.h"
 
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
 int main(void)
 {
+#ifdef LA_PIN2
+    gpio_init(LA_PIN2, GPIO_OUT);
+    gpio_clear(LA_PIN2);
+#endif
+
+#ifdef LA_PIN3
+    gpio_init(LA_PIN3, GPIO_OUT);
+    gpio_clear(LA_PIN3);
+#endif
+
+#ifdef LA_PIN4
+    gpio_init(LA_PIN4, GPIO_OUT);
+    gpio_clear(LA_PIN4);
+#endif
+
+#ifdef LA_PIN6
+    gpio_init(LA_PIN6, GPIO_OUT);
+    gpio_clear(LA_PIN6);
+#endif
+
+#ifdef LA_PIN7
+    gpio_init(LA_PIN7, GPIO_OUT);
+    gpio_clear(LA_PIN7);
+#endif
+
+#ifdef LA_PIN12
+    gpio_init(LA_PIN12, GPIO_OUT);
+    gpio_clear(LA_PIN12);
+#endif
+
+#ifdef LA_PIN13
+    gpio_init(LA_PIN13, GPIO_OUT);
+    gpio_clear(LA_PIN13);
+#endif
+
+#ifdef LA_PIN14
+    gpio_init(LA_PIN14, GPIO_OUT);
+    gpio_clear(LA_PIN14);
+#endif
+
+#ifdef LA_PIN15
+    gpio_init(LA_PIN15, GPIO_OUT);
+    gpio_clear(LA_PIN15);
+#endif
+
     /* we need a message queue for the thread running the shell in order to
      * receive potentially fast incoming networking packets */
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     puts("RIOT network stack example application");
+
+    gnrc_netif_t *netif = gnrc_netif_iter(NULL);
+    kernel_pid_t pid = netif->pid;
+
+    netopt_enable_t gts_en = NETOPT_ENABLE;
+    int res = gnrc_netapi_set(pid, NETOPT_GTS_TX, 0, &gts_en, sizeof(netopt_enable_t));
+    if (res != sizeof(netopt_enable_t)) {
+        printf("could not set GTS: %d\n", res);
+    }
+
+    netopt_enable_t ack_req_en = NETOPT_ENABLE;
+
+    res = gnrc_netapi_set(pid, NETOPT_ACK_REQ, 0, &ack_req_en, sizeof(netopt_enable_t));
+    if (res != sizeof(netopt_enable_t)) {
+        printf("could not set ACK_REQ: %d\n", res);
+    }
+
+    /* The node with ths CPU ID will be used as cordinator, all other nodes will act as RFD */
+    uint8_t coord_cpuid[CPUID_LEN] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC};
+
+    uint8_t cpuid[CPUID_LEN];
+    cpuid_get(&cpuid);
+    for (unsigned i = 0; i < CPUID_LEN; i++) {
+        printf("%02x", cpuid[i]);
+    }
+    bool is_coord = memcmp(coord_cpuid, cpuid, CPUID_LEN) == 0;
+    printf("%s\n", is_coord ? "--> COORD" : "--> RFD");
+
+    netopt_enable_t pan_coord_en = NETOPT_ENABLE;
+    if (is_coord) {
+        res = gnrc_netapi_set(netif->pid, NETOPT_PAN_COORD, 0, &pan_coord_en, sizeof(netopt_enable_t));
+        if (res != sizeof(netopt_enable_t)) {
+            printf("could not set PAN_COORD: %d\n", res);
+        }
+    }
+
+    netopt_enable_t link_en = NETOPT_ENABLE;
+    res = gnrc_netapi_set(netif->pid, NETOPT_LINK, 0, &link_en, sizeof(netopt_enable_t));
+    if (res != sizeof(netopt_enable_t)) {
+        printf("could not set LINK: %d\n", res);
+    }
 
     /* start shell */
     puts("All up, running the shell now");
